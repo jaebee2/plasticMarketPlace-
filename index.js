bdb720fc90c4f7f596760a795b08621d47f20567
jibrilbulama <40578703@live.napier.ac.uk>
2025-04-10 10:46:39 +0100

    fresh

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('this is the server');
-});
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
